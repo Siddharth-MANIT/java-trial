a52764ac909f7edcf96baa2ad72c891d8a8e12ae
Siddhath-MANIT <siddharthtomar030@gmail.com>
2025-03-10 19:32:37 +0530

    first commit

---
+++ harryC.java
@@ -0,0 +1,8 @@
+package harry;
+
+public class harryC {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+    
+}
---
+++ new.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+
+int main()
+{
+    float a;
+    a=5%3/8*3+4;
+    printf("%f",a);
+}
